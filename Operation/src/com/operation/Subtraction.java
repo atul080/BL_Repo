70ab7d5fc62d34b8b64bc5cd041b3d578c13bab4
Atul Srivastav <atul.srivastav.ap@nielsen.com>
2021-12-22 20:24:04 +0530

    Added:Sub program

---
+++ Operation/src/com/operation/Subtraction.java
@@ -0,0 +1,17 @@
+package com.operation;
+import java.util.*;
+
+public class Additon {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			Scanner sc=new Scanner(System.in);
+			int a=sc.nextInt();
+			int b=sc.nextInt();
+			int sub=a-b;
+			System.out.println("Sub is: "+sub);
+	}
+
+}
