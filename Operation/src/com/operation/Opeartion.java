26bc0f0856c15fa698da27f8402e9deec0cfb596
Atul Srivastav <atul.srivastav.ap@nielsen.com>
2021-12-22 22:32:36 +0530

    Added:Divi Prog

---
+++ Operation/src/com/operation/Opeartion.java
@@ -0,0 +1,23 @@
+package com.operation;
+import java.util.*;
+
+public class Additon {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			Scanner sc=new Scanner(System.in);
+			int a=sc.nextInt();
+			int b=sc.nextInt();
+			int sum=a-b;
+			int sub=a-b;
+			int mult=a*b;
+			int div=a/b;
+			System.out.println("Sum is: "+sum);
+			System.out.println("Sub is: "+sub);
+			System.out.println("Mult is: "+mult);
+			System.out.println("Div is: "+div);
+	}
+
+}
