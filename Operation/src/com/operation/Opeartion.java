a20d2c25ccbdb01e6cebed183be653974bbd845b
Atul Srivastav <atul.srivastav.ap@nielsen.com>
2021-12-22 22:27:07 +0530

    Added:Multiplication Prog

---
+++ Operation/src/com/operation/Opeartion.java
@@ -0,0 +1,21 @@
+package com.operation;
+import java.util.*;
+
+public class Additon {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			Scanner sc=new Scanner(System.in);
+			int a=sc.nextInt();
+			int b=sc.nextInt();
+			int sum=a-b;
+			int sub=a-b;
+			int mult=a*b;
+			System.out.println("Sum is: "+sum);
+			System.out.println("Sub is: "+sub);
+			System.out.println("Mult is: "+mult);
+	}
+
+}
