c729a6bc1e662a74d92d107d5df2d018bad04268
Atul Srivastav <atul.srivastav.ap@nielsen.com>
2021-12-22 20:19:36 +0530

    Added:Additon program

---
+++ AdditionOp/src/com/additon/Additon.java
@@ -0,0 +1,17 @@
+package com.additon;
+import java.util.*;
+
+public class Additon {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			Scanner sc=new Scanner(System.in);
+			int a=sc.nextInt();
+			int b=sc.nextInt();
+			int sum=a+b;
+			System.out.println("Sum is: "+sum);
+	}
+
+}
